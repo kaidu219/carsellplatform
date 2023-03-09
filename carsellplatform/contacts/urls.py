488d9d0f7b34fdfcbadee4a6a364e8e542da360e
Kaidu <kaidu219@gmail.com>
2023-03-09 21:06:40 +0600

    Подготовка к публикации

--- carsellplatform/contacts/urls.py
+++ carsellplatform/contacts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import inquiry, delete_inquiry
+from 
 
 
 urlpatterns = [
     path('inquiry', inquiry, name='inquiry'),
     path("<str:id>", delete_inquiry, name='delete_inquiry'),
 ]
+
